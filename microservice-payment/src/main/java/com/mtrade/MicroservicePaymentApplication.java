48040162e6001985ccb020698953a54769de770a
mohamed224 <mdiaby00@gmail.com>
2019-10-15 15:47:00 +0200

    creation of the microservice payment

---
+++ microservice-payment/src/main/java/com/mtrade/MicroservicePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.mtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePaymentApplication.class, args);
+	}
+
+}
