cdce0dd5eb25adf527ca23c4d4dce5f5c00d82a7
mohamed224 <mdiaby00@gmail.com>
2019-10-16 12:47:15 +0200

    automatic refresh using actuator

--- microservice-products/src/main/java/com/mtrade/products/configurations/AppPropertiesConfiguration.java
+++ microservice-products/src/main/java/com/mtrade/products/configurations/AppPropertiesConfiguration.java
@@ -1,11 +1,13 @@
 package com.mtrade.products.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 
 @Component
 @ConfigurationProperties("mes-configs")
+@RefreshScope
 public class AppPropertiesConfiguration {
 
     private int limitDeProduits;
