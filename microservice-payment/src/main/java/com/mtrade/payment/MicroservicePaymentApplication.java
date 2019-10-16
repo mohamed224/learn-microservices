4dc17f696d603b1e410181f7b3828fd5fbd194b2
mohamed224 <mdiaby00@gmail.com>
2019-10-16 13:20:57 +0200

    adding eureka

--- microservice-payment/src/main/java/com/mtrade/payment/MicroservicePaymentApplication.java
+++ microservice-payment/src/main/java/com/mtrade/payment/MicroservicePaymentApplication.java
@@ -2,10 +2,12 @@ package com.mtrade.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mtrade.payment")
+@EnableDiscoveryClient
 public class MicroservicePaymentApplication {
 
 	public static void main(String[] args) {
--- microservice-products/src/main/java/com/mtrade/products/MicroserviceProductsApplication.java
+++ microservice-products/src/main/java/com/mtrade/products/MicroserviceProductsApplication.java
@@ -3,8 +3,10 @@ package com.mtrade.products;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceProductsApplication {
 
 	public static void main(String[] args) {
