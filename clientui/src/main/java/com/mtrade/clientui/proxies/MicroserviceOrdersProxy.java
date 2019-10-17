82ffe297b17660fc9366c5476474b1773e466b6b
mohamed224 <mdiaby00@gmail.com>
2019-10-17 23:54:56 +0200

    adding ribbon config

--- clientui/src/main/java/com/mtrade/clientui/proxies/MicroserviceOrdersProxy.java
+++ clientui/src/main/java/com/mtrade/clientui/proxies/MicroserviceOrdersProxy.java
@@ -1,11 +1,13 @@
 package com.mtrade.clientui.proxies;
 
 import com.mtrade.clientui.beans.OrderBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "microservice-orders" ,url = "localhost:9002")
+@FeignClient(name = "microservice-orders" )
+@RibbonClient(name = "microservice-orders" )
 public interface MicroserviceOrdersProxy {
 
 
--- clientui/src/main/java/com/mtrade/clientui/proxies/MicroservicePaymentsProxy.java
+++ clientui/src/main/java/com/mtrade/clientui/proxies/MicroservicePaymentsProxy.java
@@ -1,12 +1,14 @@
 package com.mtrade.clientui.proxies;
 
 import com.mtrade.clientui.beans.PaymentBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "microservice-payments" , url = "localhost:9003")
+@FeignClient(name = "microservice-payments")
+@RibbonClient(name="microservice-payments")
 public interface MicroservicePaymentsProxy {
 
     @PostMapping("/payments")
